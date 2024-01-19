a5a39a307f2ce3ab2c4b1557ee317b1237507aaf
inji0212 <in17100170@kakao.com>
2024-01-19 12:51:31 +0900

    연구소

--- graph_14502.py
+++ graph_14502.py
@@ -9,57 +9,124 @@
 
 
 #2주변에 1을 0-0이 되게 만든다.
-# 이때 벽이 3개 이상이면 다시
+# 이때 벽이 3개 이상이면 다시 
+#실패
+
+# 바이러스 퍼트리는 함수
+# 0인부분중 벽 3개의 모든 경우를 골라 체크
+# 이중에서 가장 큰수 
 
 from collections import deque
+from itertools import combinations
+import copy
 
-def find(x,y):
-  queue = deque()
-  queue.append((x,y))
-
+def bfs(Chkgraph):
+  # 바이러스 퍼트리고 안전 영역 체크함수
+  queue=deque()
+  for i in virus:
+    queue.append(i)
+  
   while queue:
-    x,y = queue.popleft()
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-      if 0<= nx <n and 0<=ny<m :
-        continue
-      if graph[nx][ny] == 0:
-        graph[nx][ny] = 2
-        queue.append((nx,ny))
-     
-
-def area():
-  queue = deque()
-  queue.append((0,0))
-  count=0
-  while queue:
-    x,y=queue.popleft() 
-    for i in range(4):
-      nx=x+dx[i]
-      ny=y+dy[i]
-      if 0<=nx<n and 0<=ny<m and visited[nx][ny]==0 and graph[nx][ny]==0:
-        visited[nx][ny]=1
-        queue.append((nx,ny))
-        count+=1
+    x,y= queue.popleft()
+    
+    for i in range (4):
+      nx=x+ dx[i]
+      ny=y+ dy[i]
+      
+      if 0<=nx<n and 0<=ny<m:
+        if Chkgraph[nx][ny]==0:
+            Chkgraph[nx][ny]=2
+            queue.append((nx,ny))
+  
+  safeN=0
+  for i in range(n):
+    for j in range(m):
+      if Chkgraph[i][j]==0:
+        safeN+=1
         
-  return count
+  return safeN
+  
 
-
-
-n,m=map(int,input().split())
-
-visited=[[0]*m for _ in range(n)]
-graph=[]
-for _ in range(n):
-  graph.append(list(input().split()))
-
+n,m = map(int,input().split())
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
-list=[]
+
+graph=[]
+for _ in range(n):
+  graph.append(list(map(int, input().split())))
+
+area=[]
+virus=[]
 for i in range(n):
-  for j in range(m):
-    if graph[i][j]== 2:
-      list.append([i,j])
+  for j in range (m):
+    if graph[i][j]==0:
+      area.append([i,j])
+    elif graph[i][j]==2:
+      virus.append([i,j])
+result=0  
+chk=0   
+Combi_Wall=combinations(area,3)
+
+for Combi in Combi_Wall:
+    Chkgraph=copy.deepcopy(graph) 
+
+    # 3개 조합 만들기
+    for idx in range(3):
+        Chkgraph[Combi[idx][0]][Combi[idx][1]]=1
+    chk=bfs(Chkgraph)
+    if chk> result:
+      result=chk
+print(result)
+    
+#from collections import deque
+
+#def find(x,y):
+#  queue = deque()
+#  queue.append((x,y))
+
+#  while queue:
+#    x,y = queue.popleft()
+#    for i in range(4):
+#      nx = x + dx[i]
+#      ny = y + dy[i]
+#      if 0<= nx <n and 0<=ny<m :
+#        continue
+#      if graph[nx][ny] == 0:
+#        graph[nx][ny] = 2
+#        queue.append((nx,ny))
+     
+
+#def area():
+#  queue = deque()
+#  queue.append((0,0))
+#  count=0
+#  while queue:
+#    x,y=queue.popleft() 
+#    for i in range(4):
+#      nx=x+dx[i]
+#      ny=y+dy[i]
+#      if 0<=nx<n and 0<=ny<m and visited[nx][ny]==0 and graph[nx][ny]==0:
+#        visited[nx][ny]=1
+#        queue.append((nx,ny))
+#        count+=1
+        
+#  return count
+
+
+
+#n,m=map(int,input().split())
+
+#visited=[[0]*m for _ in range(n)]
+#graph=[]
+#for _ in range(n):
+#  graph.append(list(input().split()))
+
+#dx=[-1,1,0,0]
+#dy=[0,0,-1,1]
+#list=[]
+#for i in range(n):
+#  for j in range(m):
+#    if graph[i][j]== 2:
+#      list.append([i,j])
       
 
