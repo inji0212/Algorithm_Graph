2fb5163a275936ac2199781939d881afb12a628b
inji0212 <in17100170@kakao.com>
2024-01-19 10:13:37 +0900

    연구소

--- graph_14502.py
+++ graph_14502.py
@@ -1,13 +1,65 @@
 #14502
 #연구소
+#실패
 #n*m크기의 연구소 상하좌우 바이러스
+#2를 찾고 주변을 1로바꿈 => 3개이하면 남은 0에 남은 벽수 뺴기
+# 3개 이상이면 최소값찾아야함
+# 한개를 감싸고 다른 한개를 남은벽을 감싸도 갯수가 남으면 1들을 이어서 0-0이 되어야 안전
+# 너무많으면 3자리만 남아서 3개 리턴
+
+
+#2주변에 1을 0-0이 되게 만든다.
+# 이때 벽이 3개 이상이면 다시
+
 from collections import deque
+
+def find(x,y):
+  queue = deque()
+  queue.append((x,y))
+
+  while queue:
+    x,y = queue.popleft()
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if 0<= nx <n and 0<=ny<m :
+        continue
+      if graph[nx][ny] == 0:
+        graph[nx][ny] = 2
+        queue.append((nx,ny))
+     
+
+def area():
+  queue = deque()
+  queue.append((0,0))
+  count=0
+  while queue:
+    x,y=queue.popleft() 
+    for i in range(4):
+      nx=x+dx[i]
+      ny=y+dy[i]
+      if 0<=nx<n and 0<=ny<m and visited[nx][ny]==0 and graph[nx][ny]==0:
+        visited[nx][ny]=1
+        queue.append((nx,ny))
+        count+=1
+        
+  return count
+
 
 
 n,m=map(int,input().split())
-graph=[]
 
+visited=[[0]*m for _ in range(n)]
+graph=[]
 for _ in range(n):
   graph.append(list(input().split()))
 
-print(graph)
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+list=[]
+for i in range(n):
+  for j in range(m):
+    if graph[i][j]== 2:
+      list.append([i,j])
+      
+
